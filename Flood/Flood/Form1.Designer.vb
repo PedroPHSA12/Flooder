24e54fb99a6e066c5d628cad44d7049bc73a5b72
PedroPHSA12 <50556985+PedroPHSA12@users.noreply.github.com>
2019-12-14 04:06:26 -0300

    Add files via upload

---
+++ Flood/Flood/Form1.Designer.vb
@@ -0,0 +1,220 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.TextBox10 = New System.Windows.Forms.TextBox()
+        Me.TextBox11 = New System.Windows.Forms.TextBox()
+        Me.TextBox12 = New System.Windows.Forms.TextBox()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.SuspendLayout()
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 321)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(168, 64)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Sair"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(806, 311)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(168, 64)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Informações"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(2, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(463, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'Timer1
+        '
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(405, 321)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(53, 17)
+        Me.CheckBox1.TabIndex = 7
+        Me.CheckBox1.Text = "Ativar"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(405, 358)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(71, 17)
+        Me.CheckBox2.TabIndex = 8
+        Me.CheckBox2.Text = "Desativar"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(2, 38)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(463, 20)
+        Me.TextBox2.TabIndex = 9
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(2, 64)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(463, 20)
+        Me.TextBox3.TabIndex = 10
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(2, 90)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(463, 20)
+        Me.TextBox4.TabIndex = 11
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(2, 116)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(463, 20)
+        Me.TextBox5.TabIndex = 12
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(2, 142)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(463, 20)
+        Me.TextBox6.TabIndex = 13
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(511, 12)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(463, 20)
+        Me.TextBox7.TabIndex = 14
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(511, 38)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(463, 20)
+        Me.TextBox8.TabIndex = 15
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(511, 64)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(463, 20)
+        Me.TextBox9.TabIndex = 16
+        '
+        'TextBox10
+        '
+        Me.TextBox10.Location = New System.Drawing.Point(511, 90)
+        Me.TextBox10.Name = "TextBox10"
+        Me.TextBox10.Size = New System.Drawing.Size(463, 20)
+        Me.TextBox10.TabIndex = 17
+        '
+        'TextBox11
+        '
+        Me.TextBox11.Location = New System.Drawing.Point(511, 116)
+        Me.TextBox11.Name = "TextBox11"
+        Me.TextBox11.Size = New System.Drawing.Size(463, 20)
+        Me.TextBox11.TabIndex = 18
+        '
+        'TextBox12
+        '
+        Me.TextBox12.Location = New System.Drawing.Point(511, 142)
+        Me.TextBox12.Name = "TextBox12"
+        Me.TextBox12.Size = New System.Drawing.Size(463, 20)
+        Me.TextBox12.TabIndex = 19
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.ClientSize = New System.Drawing.Size(975, 387)
+        Me.Controls.Add(Me.TextBox12)
+        Me.Controls.Add(Me.TextBox11)
+        Me.Controls.Add(Me.TextBox10)
+        Me.Controls.Add(Me.TextBox9)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Flood"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents TextBox11 As TextBox
+    Friend WithEvents TextBox12 As TextBox
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+End Class
---
+++ Flood/Flood/Form1.vb
@@ -0,0 +1,83 @@
+﻿Public Class Form1
+    <System.Runtime.InteropServices.DllImportAttribute("user32.dll")>
+    Private Shared Function GetAsyncKeyState(ByVal vKey As System.Windows.Forms.Keys) As Short
+
+    End Function
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If (CheckBox1.Checked = True) Then
+            CheckBox2.Checked = False
+        Else
+            CheckBox2.Checked = True
+        End If
+        Timer1.Start()
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        If (CheckBox2.Checked = True) Then
+            CheckBox1.Checked = False
+        Else
+            CheckBox1.Checked = True
+        End If
+        Timer1.Stop()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        MsgBox("By PedroPHSA#7855(Discord)")
+        MsgBox("O Flooder pega em qualquer processo...")
+        MsgBox("Se você for usar no League Of Legends, se quiser mandar no all coloque '/all' e a mensagem que você quer,pois,ele nao vem automaticamente com o all ")
+    End Sub
+    Private Sub Timer1_Tick_1(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If (GetAsyncKeyState(Keys.F1)) Then
+            SendKeys.Send(TextBox1.Text)
+            SendKeys.Send("{Enter}")
+        End If
+        If (GetAsyncKeyState(Keys.F2)) Then
+            SendKeys.Send(TextBox2.Text)
+            SendKeys.Send("{Enter}")
+        End If
+        If (GetAsyncKeyState(Keys.F3)) Then
+            SendKeys.Send(TextBox3.Text)
+            SendKeys.Send("{Enter}")
+        End If
+        If (GetAsyncKeyState(Keys.F4)) Then
+            SendKeys.Send(TextBox4.Text)
+            SendKeys.Send("{Enter}")
+        End If
+        If (GetAsyncKeyState(Keys.F5)) Then
+            SendKeys.Send(TextBox5.Text)
+            SendKeys.Send("{Enter}")
+        End If
+        If (GetAsyncKeyState(Keys.F6)) Then
+            SendKeys.Send(TextBox6.Text)
+            SendKeys.Send("{Enter}")
+        End If
+        If (GetAsyncKeyState(Keys.F7)) Then
+            SendKeys.Send(TextBox7.Text)
+            SendKeys.Send("{Enter}")
+        End If
+        If (GetAsyncKeyState(Keys.F8)) Then
+            SendKeys.Send(TextBox8.Text)
+            SendKeys.Send("{Enter}")
+        End If
+        If (GetAsyncKeyState(Keys.F9)) Then
+            SendKeys.Send(TextBox9.Text)
+            SendKeys.Send("{Enter}")
+        End If
+        If (GetAsyncKeyState(Keys.F10)) Then
+            SendKeys.Send(TextBox10.Text)
+            SendKeys.Send("{Enter}")
+        End If
+        If (GetAsyncKeyState(Keys.F11)) Then
+            SendKeys.Send(TextBox11.Text)
+            SendKeys.Send("{Enter}")
+        End If
+        If (GetAsyncKeyState(Keys.F12)) Then
+            SendKeys.Send(TextBox12.Text)
+            SendKeys.Send("{Enter}")
+        End If
+    End Sub
+End Class
